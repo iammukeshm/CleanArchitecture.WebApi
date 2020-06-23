638f7e3239fd883bd5f4caed48291d48c9f762ff
Mukesh Murugan <iammukeshm@gmail.com>
2020-06-23 08:25:09 +0530

    ignored xml warnings

---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApi.Controllers
+{
+    public abstract class BaseApiController : Controller
+    {
+    }
+}
