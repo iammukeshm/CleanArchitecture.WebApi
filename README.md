a546dcb2bd1effe70858fdb58e60fa33c29515ae
Mukesh Murugan <iammukeshm@gmail.com>
2020-06-23 08:19:39 +0530

    added swagger ui and changed launchurl to /swagger

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Clean Architecture with ASP.NET Core 3.1 WebApi
-### Coming Soon! Work under Progress.
+### Coming Soon. Work under Progress.
 An Implementation of Clean Architecture with ASP.NET Core 3.1 WebApi
 
 ## Give a Star!
@@ -33,7 +33,7 @@ If you found this Implementation helpful or used it in your Projects, do give it
 Every changes / additions / deletions will be recorded in the [Changelog file](https://github.com/iammukeshm/CleanArchitecture.WebApi/blob/master/CHANGELOG.md).
 
 ## Support
-Having any issues or troubles getting started? [Get in touch with me](https://www.codewithmukesh.com/contact) or [raise an issue / bug / feature request] (https://github.com/iammukeshm/CleanArchitecture.WebApi/issues/new/choose). Always happy to help.
+Having any issues or troubles getting started? [Get in touch with me](https://www.codewithmukesh.com/contact) or [raise an issue / bug / feature request](https://github.com/iammukeshm/CleanArchitecture.WebApi/issues/new/choose). Always happy to help.
 
 ## Licensing
-iammukeshm/CleanArchitecture.WebApi Project is licensed with the MIT License.
+iammukeshm/CleanArchitecture.WebApi Project is licensed with the [MIT License](https://github.com/iammukeshm/CleanArchitecture.WebApi/blob/master/LICENSE).
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -10,22 +10,51 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace WebApi
 {
     public class Startup
     {
+        public IConfiguration _config { get; }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+       
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\CleanArchitecture.WebApi.xml", System.AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Clean Architecture - WebApi",
+                    Description = "This Api will be responsible for overall data distribution and authorization.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "codewithmukesh",
+                        Email = "hello@codewithmukesh.com",
+                        Url = new Uri("https://codewithmukesh.com/contact"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Input your Bearer token to access this API",
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +70,16 @@ namespace WebApi
             app.UseRouting();
 
             app.UseAuthorization();
-
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchitecture.WebApi");
+            });
+            #endregion
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
