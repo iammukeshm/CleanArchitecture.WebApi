83fda4150529963ff0332adc8b35f588bfe846ad
Bruno Moureau <bruno.moureau@hotmail.com>
2020-07-27 09:59:56 +0200

    improved unique barcode validation

--- Application/Interfaces/Repositories/IProductRepositoryAsync.cs
+++ Application/Interfaces/Repositories/IProductRepositoryAsync.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Application.Interfaces.Repositories
 {
     public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
     {
+        Task<bool> IsUniqueBarcodeAsync(string barcode);
     }
 }
