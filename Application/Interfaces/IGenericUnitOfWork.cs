02662d20c7bc135602d10fa600048e61b6d67487
hippieZhou <hippiezhou@outlook.com>
2020-09-06 19:36:10 +0800

    add GenericUnitOfWork

---
+++ Application/Interfaces/IGenericUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IGenericUnitOfWork : IDisposable
+    {
+        Task<int> Commit(CancellationToken cancellationToken);
+        Task Rollback();
+    }
+}
