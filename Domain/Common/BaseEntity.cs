bad676922e18c7186c1a4375afd8534bfa8f91c9
Wise Duho <duhowise@gmail.com>
2021-04-28 13:38:27 +0000

    updated generic repositories to factor in non integer id's added iauditable entity as base for all auditable entities. Iauditable entiity can then be used in the saved changes method to get all entries

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Domain.Common
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity<T>:IEntity<T>
+    {
+        [Key] public virtual T Id { get; set; }
+    }
+
+    public abstract class BaseEntity:IEntity<int>
     {
         public virtual int Id { get; set; }
     }
